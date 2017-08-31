bf6687299140f0cef72a49ca5277dc1f968d1601
rahulsahay19 <rahulsahay19@gmail.com>
2017-08-31 23:51:30 +0530

    Admin Menu

--- MovieReviewSPA.web/ClientApp/app/services/auth.service.ts
+++ MovieReviewSPA.web/ClientApp/app/services/auth.service.ts
@@ -1,23 +1,52 @@
 ﻿import { Injectable } from '@angular/core';
-import { tokenNotExpired } from 'angular2-jwt';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 import Auth0Lock from 'auth0-lock';
-/*import { myConfig } from './auth.config';*/
+
+
 
 // Avoid name not found warnings
 //declare var Auth0Lock: any;
 
 @Injectable()
 export class AuthService {
-    lock = new Auth0Lock('HwTjSrCFV320gGkuhZ2KiT861miuXMh1', 'movie-review.auth0.com',{});
+    profile: any;
+
+    private roles: string[] = [];
+    lock = new Auth0Lock('HwTjSrCFV320gGkuhZ2KiT861miuXMh1', 'movie-review.auth0.com', {});
 
     constructor() {
+        this.profile = JSON.parse(localStorage.getItem('profile'));
+        var token = localStorage.getItem('token');
+        
+        if (token && token.length > 25) {
+            var jwtObj = new JwtHelper();
+            var decodedToken = jwtObj.decodeToken(token);
+            this.roles = decodedToken['https://movie-review.com/roles'];
+        }
+
         // Add callback for lock `authenticated` event
         this.lock.on('authenticated', (authResult) => {
-            console.log(authResult);
             localStorage.setItem('token', authResult.accessToken);
+            //Decoding token
+            var jwtObj = new JwtHelper();
+            var decodedToken = jwtObj.decodeToken(authResult.accessToken);
+            this.roles = decodedToken['https://movie-review.com/roles'];
+            console.log("Roles:- ", this.roles);
+            this.lock.getUserInfo(authResult.accessToken,
+                (error, profile) => {
+                    if (error)
+                        throw error;
+
+                    console.log(profile);
+                    localStorage.setItem('profile', JSON.stringify(profile));
+                    this.profile = profile;
+                });
         });
     }
 
+    public isInRole(roleName) {
+        return this.roles.indexOf(roleName) > -1;
+    }
     public login() {
         // Call the show method to display the widget.
         this.lock.show();
@@ -32,5 +61,8 @@ export class AuthService {
     public logout() {
         // Remove token from localStorage
         localStorage.removeItem('token');
+        localStorage.removeItem('profile');
+        this.profile = null;
+        this.roles = [];
     };
 }
